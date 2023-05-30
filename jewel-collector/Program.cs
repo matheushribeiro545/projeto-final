87a9b434e31d9ca3a429fd21af7958231c5f2d44
fernando Cipriano Dias <fernando.cpd@outlook.com>
2023-05-29 22:23:39 -0300

    Outra Alteração feita por Fernando Dias

--- jewel-collector/Program.cs
+++ jewel-collector/Program.cs
@@ -1,3 +1,3 @@
-﻿// Alteração realizada por Fernando Cipriano Dias
+﻿// Alteração realizada por Fernando Cipriano Dias //Outra alteração realizada por Fernando Cipriano Dias
 using System;
 using jewel_collector.Models;
